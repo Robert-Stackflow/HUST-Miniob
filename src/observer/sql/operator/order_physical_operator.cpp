920b5384a7a11304a09dc8fec53660e3c676a6b2
Robert-Stackflow <72618337+Robert-Stackflow@users.noreply.github.com>
2023-12-10 00:18:01 +0800

    【Fix bug】修复order by的排序bug

--- src/observer/sql/operator/order_physical_operator.cpp
+++ src/observer/sql/operator/order_physical_operator.cpp
@@ -7,6 +7,7 @@
 #include "event/sql_debug.h"
 #include "sql/stmt/order_stmt.h"
 
+std::vector<std::vector<Tuple *>> split_tuples(std::vector<Tuple *> tuples,const TupleCellSpec &tuple_schema);
 
 RC OrderPhysicalOperator::open(Trx *trx) {
   if (children_.empty()) {
@@ -38,6 +39,10 @@ RC OrderPhysicalOperator::fetch_and_sort_table() {
     int i = 1;
   }
 
+  std::vector<std::vector<Tuple *>> tuples_list;
+
+  tuples_list.push_back(tuples_);
+
   // 排序
   for (auto &order: orders_) {
     auto table_name = order->field().table()->name();
@@ -45,21 +50,66 @@ RC OrderPhysicalOperator::fetch_and_sort_table() {
     auto tuple_schema = TupleCellSpec(table_name, field_name);
     auto order_type = order->type();
 
-    std::sort(tuples_.begin(), tuples_.end(), [&](Tuple *left, Tuple *right) {
-      Value left_value;
-      left->find_cell(tuple_schema, left_value);
-      Value right_value;
-      right->find_cell(tuple_schema, right_value);
-      int res = left_value.compare(right_value);
-      if (order_type == ASC) {
-        return res < 0;
+    std::vector<std::vector<Tuple *>> tmp;
+
+    for(auto tuples:tuples_list){
+      std::sort(tuples.begin(), tuples.end(), [&](Tuple *left, Tuple *right) {
+        Value left_value;
+        left->find_cell(tuple_schema, left_value);
+        Value right_value;
+        right->find_cell(tuple_schema, right_value);
+        int res = left_value.compare(right_value);
+        if (order_type == ASC) {
+          return res < 0;
+        }
+        return res > 0;
+      });
+
+      auto splited_tuples=split_tuples(tuples,tuple_schema);
+      for(auto t : splited_tuples){
+        tmp.push_back(t);
       }
-      return res > 0;
-    });
+    }
+    
+    tuples_list.swap(tmp);
   }
+
+  std::vector<Tuple *> tmp_tuples;
+
+  for(auto tuples:tuples_list){
+    for(auto tuple:tuples){
+      tmp_tuples.push_back(tuple);
+    }
+  }
+
+  tuples_.swap(tmp_tuples);
 
   index_ = -1;
   return RC::SUCCESS;
+}
+
+std::vector<std::vector<Tuple *>> split_tuples(std::vector<Tuple *> tuples,const TupleCellSpec &tuple_schema){
+  Value last_value(NULLS);
+  Value cur_value(NULLS);
+  std::vector<Tuple*> cur_tuples;
+  std::vector<std::vector<Tuple *>> tuples_list;
+  for(auto tuple : tuples){
+    tuple->find_cell(tuple_schema,cur_value);
+    if(last_value.attr_type()==NULLS || last_value.compare(cur_value)==0){
+      cur_tuples.push_back(tuple);
+      last_value = cur_value;
+    }else if(last_value.compare(cur_value)!=0){
+      std::vector<Tuple*> tmp;
+      tmp.swap(cur_tuples);
+      tuples_list.push_back(tmp);
+      cur_tuples.push_back(tuple);
+      last_value = cur_value;
+    }
+  }
+  if(cur_tuples.size()>0){
+    tuples_list.push_back(cur_tuples);
+  }
+  return tuples_list;
 }
 
 RC OrderPhysicalOperator::next() {
